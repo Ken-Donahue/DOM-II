802a427b4a9eab6dab5155a03d1370ace5d2b746
Ken Donahue <ken.w.donahue@gmail.com>
2020-04-08 00:50:12 -0400

    Only thing left is .stopPropagation

--- js/index.js
+++ js/index.js
@@ -2,63 +2,90 @@ let get = (selector)=>{
     return document.querySelector(selector);
 }
 
-let body = get('body');
+
+let body = get('.container.home');
 let logo = get('.logo-heading');
 let navLinks = document.querySelectorAll('a');
+let image = document.querySelectorAll('img');
+
+
+function getRandomColor(){
+    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+
 
 function mouseOver(){
-    logo.style.color = 'orangered';
+    logo.style.color = getRandomColor();
+    logo.style.background = getRandomColor();
+    logo.style.borderRadius = '5px';
 }
 
 function mouseOut(){
     logo.style.color = '#212529';
+    logo.style.background = 'white';
 }
 
 logo.addEventListener('mouseover', mouseOver);
 logo.addEventListener('mouseout', mouseOut);
 
+
 function keyDown(){
-    body.style.backgroundColor = 'skyblue';
+    body.style.background = 'white';;
 }
 
 function keyUp(){
-    body.style.backgroundColor = 'white';
+    body.style.color = '#212529';
 }
 
-body.addEventListener('keydown', keyDown);
-body.addEventListener('keyup', keyUp);
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
 
 function scroll(){
-    body.style.backgroundColor = 'skyblue';
-
-    setTimeout(()=>{
-        body.style.backgroundColor = 'white';
-    }, 1000);
+    body.style.background = getRandomColor();
+    body.style.color = getRandomColor();
 }
 
 body.addEventListener('wheel', scroll);
 
+
 navLinks.forEach(a=>{
     a.addEventListener('mousedown', (e)=>{
-        e.target.style.backgroundColor = 'skyblue';
-        e.target.style.color = 'white';
+        e.target.style.backgroundColor = getRandomColor();
+        e.target.style.color = getRandomColor();
+        
+        setTimeout(()=>{
+            e.target.style.backgroundColor = 'white';
+            e.target.style.color = '#212529';
+        }, 1500);
     });
 
     a.addEventListener('mouseup', (e)=>{
         e.target.style.backgroundColor = 'white';
         e.target.style.color = '#212529';
     });
+
+    a.addEventListener('click', (e)=>{
+        e.preventDefault();
+    });
 })
 
-// function mouseDown(){
-//     navLinks.style.backgroundColor = 'skyblue';
-//     navLinks.style.color = 'white';
-// }
 
-// function mouseUp(){
-//     navLinks.style.backgroundColor = 'white';
-//     navLinks.style.color = '#212529';
-// }
+window.addEventListener('resize', ()=>{
+    body.style.color = getRandomColor();
+    body.style.background = getRandomColor();
+});
 
-// navLinks.addEventListener('mousedown', mouseDown);
-// navLinks.addEventListener('mouseup', mouseUp);
+
+
+image.forEach(img=>{
+    img.addEventListener('dblclick', ()=>{
+        if (img.style.transform === '') {
+            img.style.transform = 'rotate(-180deg)';
+        } 
+        else {
+            img.style.transform = '';
+        }
+    });
+})
